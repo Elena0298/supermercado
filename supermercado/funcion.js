eff9157804e12500266f3c4c42db839b806a5cc9
Elena0298 <54298127+Elena0298@users.noreply.github.com>
2024-02-01 17:48:45 -0600

    Página web
    Subiendo archivos de la página web

---
+++ supermercado/funcion.js
@@ -0,0 +1,9 @@
+document.getElementById('departamento-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'departamento.html'; 
+});
+
+document.getElementById('inicio-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'index.html'; 
+});
